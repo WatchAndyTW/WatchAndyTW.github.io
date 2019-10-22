ff065fef5f19c1f9c10ef22cfbae6e5ba6ff89c6
WatchAndyTW <51250139+WatchAndyTW@users.noreply.github.com>
2019-10-22 17:00:07 +0800

    Delete console.js

--- bot/handlers/console.js
+++
@@ -1,7 +0,0 @@
-module.exports = (bot) => {
-let prompt = process.openStdin()
-prompt.addListener("data", res => {
-    let x = res.toString().trim().split(/ +/g)
-        bot.channels.get("610996015998435340").send(x.join(" "));
-    });
-}
